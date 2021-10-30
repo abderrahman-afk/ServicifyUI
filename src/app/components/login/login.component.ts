159748abaa306563a40b5d6fa55ce2ccc1480021
abderrahman khalladi <81231522+abderrahman-afk@users.noreply.github.com>
2021-10-30 17:05:26 +0200

    login fom Template drive, form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginform:FormGroup;
+  user:any={};
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit() {
-  }
+    this.loginform=this.fb.group({
+      username:[''],
+      password:['']
 
+    })
+  }
+login(){
+  console.log("hi user",this.user);
+  
+}
 }
