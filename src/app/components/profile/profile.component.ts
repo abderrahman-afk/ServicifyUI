7193a2e1b07f35de5b67b7e69c96df0ece0b8837
chocofoxy <hamzus007jouini@gmail.com>
2021-12-06 22:02:08 +0100

    d

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
   test:boolean=true;
-  constructor() { }
+  user = {}
+  profile = {}
+
+  constructor(private userService:UserService) {}
 
   ngOnInit() {
+    this.user = this.userService.getUser();
   }
 
   edit(){
